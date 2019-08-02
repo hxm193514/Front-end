7989ff7955f02b2844ace3866555c8ad203281f9
hxm193514 <1935197914@qq.com>
2019-08-02 18:02:44 +0800

    原生js扫雷小游戏

--- 原生js/扫雷小游戏/demo.js
+++ 原生js/扫雷小游戏/demo.js
@@ -6,19 +6,26 @@
 var starBtn = document.getElementById("starBtn");
 var box = document.getElementById("box");
 var promptText = document.getElementById('promptText');
+var amount = document.getElementById('amount'); //雷的数量
+var cpm = document.getElementById('cpm');
+var boxcpm = document.getElementById('boxcpm');
+var close1 = document.getElementById('close');
 var minesNum;
 var mineOver;
 var grid;
 var mineMap = [];
-var isLei;
+var isLei = document.getElementsByClassName('isLei');
 bindEvent();
-
+var startGameBool = true;
 //开始绑定的事件
 function bindEvent() {
     starBtn.onclick = function () {
-        box.style.display = 'block';
-        promptText.style.display = 'block';
-        init();
+        if (startGameBool == true) {
+            box.style.display = 'block';
+            promptText.style.display = 'block';
+            init();
+            startGameBool= false;
+        }
     }
     box.oncontextmenu = function () {
         return false;
@@ -30,6 +37,9 @@ function bindEvent() {
         } else if (e.which == 3) {
             rightClick(event);
         }
+    }
+    close1.onclick = function(){
+        close();
     }
 
 }
@@ -49,27 +59,85 @@ function init() {
         }
     }
     grid = document.getElementsByClassName('grid');
-
-    for (var m = 0; m < minesNum; m++) {
-        var mineIndex = Math.floor(Math.random() * grid.length);
+    while (minesNum) {
+        var mineIndex = Math.floor(Math.random() * 100);
         if (mineMap[mineIndex].mine === 0) {
             mineMap[mineIndex].mine = 1;
             grid[mineIndex].classList.add('isLei');
+            minesNum--;
         }
     }
+    mineOver = isLei.length;
+    amount.innerHTML = mineOver;
 }
 
 function leftClick(dom) {
-    isLei = document.getElementsByClassName('isLei');
     if (dom && dom.classList.contains('isLei')) {
         for (var i = 0; i < isLei.length; i++) {
             isLei[i].classList.add('show');
         }
+        setTimeout(function() {
+            boxcpm.style.display = 'block';
+            cpm.style.backgroundImage = "url(img/over.jpg)";
+        }, 1000);
+      
+    } else {
+        var n = 0;
+        var posArr = dom && dom.getAttribute('id').split('-');
+        var posX = posArr && +posArr[0];
+        var posY = posArr && +posArr[1];
+        dom && dom.classList.add('num');
+        for (var i = posX - 1; i <= posX + 1; i++) {
+            for (var j = posY - 1; j <= posY + 1; j++) {
+                var aroudBox = document.getElementById(i + '-' + j);
+                if (aroudBox && aroudBox.classList.contains('isLei')) {
+                    n++;
+                }
+            }
+        }
+        dom && (dom.innerHTML = n);
+        if (n == 0) {
+            for (var i = posX - 1; i <= posX + 1; i++) {
+                for (var j = posY - 1; j <= posY + 1; j++) {
+                    var nearBox = document.getElementById(i + '-' + j);
+                    if (nearBox && nearBox.length != 0) {
+                        if (!nearBox.classList.contains('check')) {
+                            nearBox.classList.add('check');
+                            leftClick(nearBox);
+                        }
+                    }
+                }
+            }
+        }
     }
 }
-function rightClick(dom){
-    isLei = document.getElementsByClassName('isLei');
-    if (dom && dom.classList.contains('isLei')) {
-            isLei[i].classList.add('sign');
+
+function rightClick(dom) {
+
+    if (dom.classList.contains('num')) {
+        return;
     }
+    dom.classList.toggle('sign');
+    if (dom.classList.contains('isLei') && dom.classList.contains('sign')) {
+        mineOver--;
+            if(mineOver==0)
+            {
+                boxcpm.style.display = 'block';
+                cpm.style.backgroundImage = "url(img/success.png)";
+            }
+    }
+    if (dom.classList.contains('isLei') && !dom.classList.contains('sign')) {
+        mineOver++;
+    }
+   
+    amount.innerHTML = mineOver;
+}
+function close(){
+    isLei.length;
+    boxcpm.style.display = 'none';
+    box.style.display = 'none';
+    box.innerHTML = '';
+  
+    promptText.style.display = 'none';
+    startGameBool = true;
 }
